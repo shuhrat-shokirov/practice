a1d65db913635b910f9b31e393939c07fa1bd1b9
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-11 11:59:14 +0300

    Convert slice to clice array

--- main.go
+++ main.go
@@ -2,78 +2,71 @@ package main
 
 func main() {
 }
-func StoreRevenue(firstStore, secondStore, thirdStore []int) (IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue []int){
-	maxBestDailyRevenue := firstStore[0]
-	minWorstDailyRevenue := firstStore[0]
+func storeRevenue(stores [][]int) (indexOfMaxRevenue, indexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue []int) {
+	maxBestDailyRevenue := stores[0][0]
+	minWorstDailyRevenue := stores[0][0]
 	maxRevenue := 0
 	maxAverageDailyRevenue := 0
-	maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue = searchRevenue(firstStore, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue)
-	maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue = searchRevenue(secondStore, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue)
-	maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue = searchRevenue(thirdStore, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue)
-	increment := 1
-	//IndexOfMaxRevenue1, IndexOfAverageDailyRevenue1, indexTheBestDailyRevenue1, indexTheWorstDailyRevenue1 := []int{}
-	IndexOfMaxRevenue1, IndexOfAverageDailyRevenue1, indexTheBestDailyRevenue1, indexTheWorstDailyRevenue1 := employeeComparison(increment, firstStore,maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue,IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue)
-	increment++
-	IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue = assignment(IndexOfMaxRevenue1,IndexOfMaxRevenue), assignment(IndexOfAverageDailyRevenue1,IndexOfAverageDailyRevenue), assignment(indexTheBestDailyRevenue1, indexTheBestDailyRevenue), assignment(indexTheWorstDailyRevenue1, indexTheWorstDailyRevenue)
-	IndexOfMaxRevenue1, IndexOfAverageDailyRevenue1, indexTheBestDailyRevenue1, indexTheWorstDailyRevenue1 = employeeComparison(increment, secondStore,maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue, IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue)
-	increment++
-	IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue = assignment(IndexOfMaxRevenue1,IndexOfMaxRevenue), assignment(IndexOfAverageDailyRevenue1,IndexOfAverageDailyRevenue), assignment(indexTheBestDailyRevenue1, indexTheBestDailyRevenue), assignment(indexTheWorstDailyRevenue1, indexTheWorstDailyRevenue)
-	IndexOfMaxRevenue1, IndexOfAverageDailyRevenue1, indexTheBestDailyRevenue1, indexTheWorstDailyRevenue1 = employeeComparison(increment, thirdStore,maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue, IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue)
-	IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue = assignment(IndexOfMaxRevenue1,IndexOfMaxRevenue), assignment(IndexOfAverageDailyRevenue1,IndexOfAverageDailyRevenue), assignment(indexTheBestDailyRevenue1, indexTheBestDailyRevenue), assignment(indexTheWorstDailyRevenue1, indexTheWorstDailyRevenue)
+	indexMax :=0
+	indexAverageDaily := 0
+	indexBestBaily := 0
+	indexWorstDaily := 0
+	for _, store := range stores {
+		searchRevenue(store, &maxBestDailyRevenue, &minWorstDailyRevenue, &maxRevenue, &maxAverageDailyRevenue)
+	}
+	for index, store := range stores {
+		indexMax, indexAverageDaily, indexBestBaily, indexWorstDaily = employeeComparison(index, store, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue)
+		assignment(indexMax, &indexOfMaxRevenue)
+		assignment(indexAverageDaily, &indexOfAverageDailyRevenue)
+		assignment(indexBestBaily, &indexTheBestDailyRevenue)
+		assignment(indexWorstDaily, &indexTheWorstDailyRevenue)
+	}
 	return
 }
-func searchRevenue(openingHours []int, maxBestDailyRevenue1, minWorstDailyRevenue1, maxRevenue1, maxAverageDailyRevenue1  int) (maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue int){
+func searchRevenue(openingHours []int, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue *int)  {
 	revenue := 0
-	maxBestDailyRevenue = maxBestDailyRevenue1
-	minWorstDailyRevenue = minWorstDailyRevenue1
-	maxRevenue = maxRevenue1
-	maxAverageDailyRevenue = maxAverageDailyRevenue1
-	for _, value := range openingHours{
-		if maxBestDailyRevenue < value{
-			maxBestDailyRevenue = value
+	for _, value := range openingHours {
+		if *maxBestDailyRevenue < value {
+			*maxBestDailyRevenue = value
 		}
-		if minWorstDailyRevenue > value {
-			minWorstDailyRevenue = value
+		if *minWorstDailyRevenue > value {
+			*minWorstDailyRevenue = value
 		}
 		revenue += value
 	}
-	if maxRevenue < revenue{
-		maxRevenue = revenue
+	if *maxRevenue < revenue {
+		*maxRevenue = revenue
 	}
-	if revenue / len(openingHours) > maxAverageDailyRevenue {
-		maxAverageDailyRevenue = revenue / len(openingHours)
+	if revenue/len(openingHours) > *maxAverageDailyRevenue {
+		*maxAverageDailyRevenue = revenue / len(openingHours)
 	}
-	return
 }
-func employeeComparison(increment int, comparisonOfValue []int, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue int, IndexOfMaxRevenue, IndexOfAverageDailyRevenue, indexTheBestDailyRevenue, indexTheWorstDailyRevenue []int) (IndexOfMaxRevenue1, IndexOfAverageDailyRevenue1, indexTheBestDailyRevenue1, indexTheWorstDailyRevenue1 int){
+func employeeComparison(index int, comparisonOfValue []int, maxBestDailyRevenue, minWorstDailyRevenue, maxRevenue, maxAverageDailyRevenue int) (indexMax, indexAverageDaily, indexBestBaily, indexWorstDaily int) {
 	revenue := 0
-	quantityInOneEmployee := 0
-	quantityInOneEmployee1 := 0
-	//Для первого работника
-	for _, search := range comparisonOfValue{
+	quantityInOneEmployeeForMax := 0
+	quantityInOneEmployeeForMin := 0
+	for _, search := range comparisonOfValue {
 		revenue += search
-		if maxBestDailyRevenue == search && quantityInOneEmployee1 == 0{
-			indexTheBestDailyRevenue1 = increment
-			quantityInOneEmployee1++
+		if maxBestDailyRevenue == search && quantityInOneEmployeeForMax == 0 {
+			indexBestBaily = index + 1
+			quantityInOneEmployeeForMax++
 		}
-		if minWorstDailyRevenue == search && quantityInOneEmployee == 0{
-			indexTheWorstDailyRevenue1 =  increment
-			quantityInOneEmployee++
+		if minWorstDailyRevenue == search && quantityInOneEmployeeForMin == 0 {
+			indexWorstDaily = index + 1
+			quantityInOneEmployeeForMin++
 		}
 	}
-	if revenue == maxRevenue{
-		IndexOfMaxRevenue1 =increment
+	if revenue == maxRevenue {
+		indexMax = index + 1
 	}
 	maxAverage := revenue / len(comparisonOfValue)
-	if maxAverage == maxAverageDailyRevenue{
-		IndexOfAverageDailyRevenue1 =  increment
+	if maxAverage == maxAverageDailyRevenue {
+		indexAverageDaily = index + 1
 	}
 	return
 }
-func assignment( value int, values []int) []int{
-	if value !=0 {
-	values = append(values, value)
-	return values
+func assignment(value int, values *[]int) {
+	if value != 0 {
+		*values = append(*values, value )
 	}
-	return values
 }
--- main_test.go
+++ main_test.go
@@ -8,21 +8,19 @@ import (
 func TestStoreRevenue(t *testing.T) {
 	tests := []struct {
 		name                           string
-		firstRevenue                   []int
-		secondRevenue				   []int
-		thirdRevenue				   []int
+		Revenues                       [][]int
 		wantIndexOfMaxRevenue          []int
 		wantIndexOfAverageDailyRevenue []int
 		wantIndexTheBestDailyRevenue   []int
 		wantIndexTheWorstDailyRevenue  []int
 	}{
-		{"If all employees work equally",[]int{10, 10, 10, 15},[]int{10, 10, 10, 15}, []int{10, 10, 10, 15},[]int{1, 2, 3}, []int{1, 2, 3}, []int{1, 2, 3},[]int{1, 2, 3}},
-		{"Workers worked differently",[]int{7, 1, 2, 3},[]int{10, 10, 10, 15}, []int{7, 20, 2},[]int{2}, []int{2}, []int{3},[]int{1}},
-		{"Some days workers converge",[]int{7, 1, 2, 3},[]int{10, 10, 10, 15}, []int{7, 1, 2},[]int{2}, []int{2}, []int{2},[]int{1, 3}},
+		{"If all employees work equally",[][]int{{10, 10, 10, 15}, {10, 10, 10, 15}, {10, 10, 10, 15}},[]int{1, 2, 3}, []int{1, 2, 3}, []int{1, 2, 3},[]int{1, 2, 3}},
+		{"Workers worked differently",[][]int{{7, 1, 2, 3}, {10, 10, 10, 15}, {7, 20, 2}},[]int{2}, []int{2}, []int{3},[]int{1}},
+		{"Some days workers converge",[][]int{{7, 1, 2, 3}, {10, 10, 10, 15}, {7, 1, 2}},[]int{2}, []int{2}, []int{2},[]int{1, 3}},
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-			gotIndexOfMaxRevenue, gotIndexOfAverageDailyRevenue, gotIndexTheBestDailyRevenue, gotIndexTheWorstDailyRevenue := StoreRevenue(tt.firstRevenue, tt.secondRevenue, tt.thirdRevenue)
+			gotIndexOfMaxRevenue, gotIndexOfAverageDailyRevenue, gotIndexTheBestDailyRevenue, gotIndexTheWorstDailyRevenue := storeRevenue(tt.Revenues)
 			if !reflect.DeepEqual(gotIndexOfMaxRevenue, tt.wantIndexOfMaxRevenue) {
 				t.Error("convert:", tt.name, "want:", tt.wantIndexOfMaxRevenue, "got:", gotIndexOfMaxRevenue)
 			}
